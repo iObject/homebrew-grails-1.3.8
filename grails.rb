d3f96a77695834c1df0aff3a820e2e48401c87fe
Tyler Smith <tsmith@smith-2.local>
2013-10-18 11:11:04 -0300

    Initial Commit

---
+++ grails.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Grails < Formula
+  homepage 'http://grails.org'
+  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-1.3.8.zip'
+  md5 'd28ac73ae160ce512f7f0c957f895995'
+
+  def install
+    rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
+    prefix.install_metafiles
+    libexec.install Dir['*']
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats; <<-EOS.undent
+    The GRAILS_HOME directory is:
+      #{opt_prefix}/libexec
+    EOS
+  end
+end
